2051378494628d54af049f225de66ff80b1ff84c
Oleksandr Siukov <as-service2008@ukr.net>
2023-03-11 12:34:12 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+refs.startBtn.addEventListener('click', onChangeBackGround);
+refs.stopBtn.addEventListener('click', offChangeBackGround);
+
+function onChangeBackGround() {
+  //   timerID = setInterval(changeBackGround, 1000);
+  timerNomber = setInterval(changeBackGround, 1000);
+  // console.log(timerID);
+}
+
+function offChangeBackGround() {
+  // console.log(timerID);
+  //   clearInterval(timerID);
+  clearInterval(timerNomber);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBackGround() {
+  refs.body.style.background = getRandomHexColor();
+}
