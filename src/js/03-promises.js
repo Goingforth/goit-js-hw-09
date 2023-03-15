8a85f3fae9df6385a6b0f14b6413e8d7e5322cd4
Oleksandr Siukov <as-service2008@ukr.net>
2023-03-15 13:31:51 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -11,7 +11,7 @@ function onFormSubmit(event) {
   const step = Number(formEl.elements.step.value);
   const amount = Number(formEl.elements.amount.value);
 
-  for (position = 1; position <= amount; position += 1) {
+  for (let position = 1; position <= amount; position += 1) {
     createPromise(position, delay)
       .then(({ position, delay }) => {
         Notiflix.Notify.success(
